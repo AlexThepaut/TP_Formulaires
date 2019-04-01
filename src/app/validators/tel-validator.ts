6ce38098cabee88086002747fc438bc178d2d1c0
Alex <alex.thepaut33@gmail.com>
2019-04-01 17:14:04 +0200

    Tel validator à fixer

--- src/app/validators/tel-validator.ts
+++ src/app/validators/tel-validator.ts
@@ -8,5 +8,6 @@ import _ from 'lodash';
 export function isTelValidValidator(control: FormControl) {
     const regexTel1 = /([0-9]{2}( |-|)){4}[0-9]{2}/g;
     const regexTel2 = /^[0]/g;
-    return (regexTel1.test(control.value) && regexTel2.test(control.value)) ? null : { isTelValid: true };
+    console.log(regexTel1.test(control.value) && regexTel2.test(control.value))
+    return (control.value == '' || (regexTel1.test(control.value) && regexTel2.test(control.value))) ? null : { isTelValid: true };
 };
